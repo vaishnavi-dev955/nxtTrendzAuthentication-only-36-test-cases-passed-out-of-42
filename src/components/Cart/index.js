03fb640fe2b1edfb395159c885e823581a361a51
vaishnavi-dev955 <vaishnavimandala1998@gmail.com>
2023-02-16 17:30:22 +0000

    only 36 test cases passed out of 42

---
+++ src/components/Cart/index.js
@@ -0,0 +1,14 @@
+// Write your JS code here
+import './index.css'
+
+const Cart = () => (
+  <div className="Cart-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+      alt="cart"
+      className="cart-style"
+    />
+  </div>
+)
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,14 @@
+// Write your JS code here
+import './index.css'
+
+const Products = () => (
+  <div className="Product-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+      alt="products"
+      className="product-style"
+    />
+  </div>
+)
+
+export default Products
